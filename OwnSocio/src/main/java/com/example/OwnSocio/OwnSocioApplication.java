c1683c4fdbd9052ef1a42fcbee5524f65bdde925
Shubhammm14 <shubhamshaw905@gmail.com>
2024-02-24 01:12:07 +0530

    set up

---
+++ OwnSocio/src/main/java/com/example/OwnSocio/OwnSocioApplication.java
@@ -0,0 +1,13 @@
+package com.example.OwnSocio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnSocioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnSocioApplication.class, args);
+	}
+
+}
---
+++ OwnSocio/src/test/java/com/example/OwnSocio/OwnSocioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OwnSocio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnSocioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
